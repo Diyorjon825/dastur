568c0c5761fdea82959826d009c50149e416a960
Macbook <macbook@MBP-Macbook.netis>
2021-05-15 14:24:01 +0500

    dart application bilan ishlayapman

--- dart_application_1/lib/dart_application_1.dart
+++ dart_application_1/lib/dart_application_1.dart
@@ -1,3 +1,15 @@
-int calculate() {
-  return 6 * 7;
+void main() {
+  Diyor diyor = new Diyor(8, 25);
+  diyor.printMe();
+}
+
+class Diyor {
+  var a, b;
+  Diyor(var a, var b) {
+    this.a = a;
+    this.b = b;
+  }
+  void printMe() {
+    print('$a+$b=${a + b}');
+  }
 }
